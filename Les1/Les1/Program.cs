283d7f7c833e121c6768846e35ccf4c907d4952e
Martin <1665634lam@zuyd.nl>
2018-09-11 13:37:49 +0200

    Les 1

---
+++ Les1/Les1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Dit is een test");
+            //Console.ReadKey();
+
+            //string tekst;
+            //tekst = "Hallo";
+            //Console.WriteLine(tekst);
+            //Console.ReadKey();
+
+
+            //string input;
+            //input = Console.ReadLine();
+            //int input2;
+            //input2 = Int32.Parse(input);
+            //Console.WriteLine(input2);
+            //Console.ReadKey();
+
+            //string input = Console.ReadLine();
+            //int input2 = Int32.Parse(input);
+            //Console.WriteLine(input2);
+            //Console.ReadKey();
+
+
+            //string getal = Console.ReadLine();
+            //int getal1 = Int32.Parse(getal);
+            //Console.WriteLine(getal1);
+            //string getal2 = Console.ReadLine();
+            //int getal3 = Int32.Parse(getal2);
+
+            //Console.WriteLine(getal3);
+            //Console.WriteLine(getal1 + getal3);
+
+            //console.readkey();
+
+
+            /*
+            Console.WriteLine("Geef je leeftijd op");
+            string input = Console.ReadLine();
+            int input2 = Int32.Parse(input);
+
+            
+            if (input2 > 18)
+            {
+                Console.WriteLine("Je mag stemmen");
+            }
+            else
+            {
+                Console.WriteLine("Je bent nog te jong");
+            }
+            Console.ReadKey();
+            */
+
+            /*
+            if (a == b)
+            {
+
+            }
+
+            else if (a < b)
+            {
+
+            }
+
+            else
+            {
+
+            }
+
+            for (int i; i < x; i++)
+            {
+
+            }
+            */
+
+
+
+            //int x = 3;
+            //for (int i = 0; i < x; i++)
+            //{
+            //    Console.WriteLine("Hallo");
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+
+            //int A = 5;
+            //do
+            //{
+            //    Console.WriteLine("Ja");
+            //    A += 1;
+            //}
+            //while (A < 6);
+            //Console.ReadKey();
+
+
+            string getal = Console.ReadLine();
+            int getal1 = Int32.Parse(getal);
+            Console.WriteLine(getal1);
+
+            string getal2 = Console.ReadLine();
+            int getal3 = Int32.Parse(getal2);
+            Console.WriteLine(getal3);
+
+            Console.WriteLine(getal1 + getal3);
+
+            Console.ReadKey();
+
+        }
+    }
+}
